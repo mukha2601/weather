f1e4d9a29b075ca232cd2d3910beebffffb4f7f7
mukha <muhammaddin.xoliqov1998@gmail.com>
2024-02-27 19:55:44 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Region from "./components/Region.vue";
+import Weather from "./components/Weather.vue";
+</script>
+
+<template>
+  <div class="app grid md:grid-cols-3 w-full h-screen">
+    <Weather class="w-full border-2 col-span-2" />
+    <Region class="w-full border-2" />
+  </div>
+</template>
+
+<style scoped>
+/* .app {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+} */
+</style>
